2992e2e98d35c2af9c052c965247c9ec413a59a9
jvander2 <jvander2@ncsu.edu>
2015-10-10 13:08:08 -0400

    Just getting Git set up

---
+++ CallQueue/src/data/Ticket.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package data;
+
+/**
+ * @author Jeremy
+ *
+ */
+public class Ticket implements Comparable<Ticket> {
+
+	@Override
+	public int compareTo(Ticket arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
